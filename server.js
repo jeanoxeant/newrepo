bc75fe0336b7d0cbdc1f248dd69d650a514808ee
jeanoxeant <jeanrubinoxeant@gmail.com>
2025-09-18 06:29:55 +0200

    fixing error in my model folder.

--- server.js
+++ server.js
@@ -5,11 +5,14 @@
 /* ***********************
  * Require Statements
  *************************/
+const inventoryRoute = require("./routes/inventoryRoute")
+const baseController = require("./controllers/baseController")
 const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const utilities = require("./utilities/")
 
 
 /* ***********************
@@ -23,11 +26,10 @@ app.set("layout", "./layouts/layout") // not at views root
  * Routes
  *************************/
 app.use(static)
-
 // Index route
-app.get("/", function(req, res){
-  res.render("index", {title: "Home"})
-})
+app.get("/", baseController.buildHome) 
+// Inventory routes
+app.use("/inv", inventoryRoute)
 
 /* ***********************
  * Local Server Information
